46922f574b9c077260dc435180b20a9db8b3ba42
OneSpringCloud <cloud.onespring@gmail.com>
2025-04-08 16:40:35 +0900

    boj10951

---
+++ src/boj10951_ApBm4_104ms.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class boj10951_ApBm4_104ms {
+    public static void main(String[] args) throws Exception{
+        // BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        StringBuilder sb = new StringBuilder();
+
+        while((line = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(line);
+            int A = Integer.parseInt(st.nextToken()); // 0 < A,B < 10
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A+B).append("\n");
+        }
+
+        System.out.println(sb.toString());
+        // br.close();
+    }
+}
+₩
